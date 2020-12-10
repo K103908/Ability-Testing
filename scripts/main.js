7195d42d98d17769e2e6a82ab6aeefeeff95636f
MEEP of Faith <meepmatthew@gmail.com>
2020-12-09 23:03:29 -0800

    fix

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+//This'll be needed if there's more than one script
+require("units/carrier")
--- scripts/units/carrier.js
+++ scripts/units/carrier.js
@@ -1,15 +1,10 @@
-const SuNavT3 =
-extendContent (UnitType,
-"Midway", {}) ;
-SuNavT3.constructor = () =>
-extend(UnitWaterMove, {});
-SuNavT3.abilities.add(new
-UnitSpawnAbility(UnitTypes.horizon,
-60 * 30, 0, -7.5));
-SuNavT3.abilities.add(new
-UnitSpawnAbility(UnitTypes.flare,
--60 * -30, 0, 7.5));
-SuNavT3.abilties.add(new
-StatusFieldAbility(StatusEffects
-.overclock, 60 * 6, 60 * 9, 
-64));
+const midway = extendContent (UnitType, "Midway", {});
+midway.constructor = () => extend(UnitWaterMove, {});
+//2 ain't enough. time to m u l t i p l y
+for(var i = 0; i < 5; i++){
+  //Unit Spawn Ability(unit, spawn time, x, y)
+  midway.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 60 * 30, 0, -7.5));
+  midway.abilities.add(new UnitSpawnAbility(UnitTypes.flare, -60 * -30, 0, 7.5));
+}
+//StatsFieldAbility(effect, duration, reload, radius)
+midway.abilties.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
