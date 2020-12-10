357837e26f6b5b13e80248449b188574b055ab8a
MEEP of Faith <meepmatthew@gmail.com>
2020-12-09 23:16:28 -0800

    Perfect spawn locations

--- scripts/units/carrier.js
+++ scripts/units/carrier.js
@@ -1,11 +1,11 @@
 const midway = extendContent (UnitType, "Midway", {});
 midway.constructor = () => extend(UnitWaterMove, {});
 //2 ain't enough. time to m u l t i p l y
-var sides = [0, -7.5];
+var sides = [-8, 8];
 for(var i = 0; i < 5; i++){
   //Unit Spawn Ability(unit, spawn time, x, y)
-  midway.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 60 * 30, sides[i % 1], 15 - i * 7.5));
-  midway.abilities.add(new UnitSpawnAbility(UnitTypes.flare, 60 * 30, sides[(i + 1) % 1], 15 - i * 7.5));
+  midway.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 60 * 30 + 60 * i, sides[i % 2], 32 - i * 16));
+  midway.abilities.add(new UnitSpawnAbility(UnitTypes.flare, 60 * 30 + 60 * i, sides[(i + 2) % 2], 32 - i * 16));
 }
 //StatsFieldAbility(effect, duration, reload, radius)
 midway.abilties.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
