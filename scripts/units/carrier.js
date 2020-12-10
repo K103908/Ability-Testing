0b5a6f1147d348de5efd0412501bc5ee9985ecd7
MEEP of Faith <meepmatthew@gmail.com>
2020-12-09 23:08:52 -0800

    better spawn position

--- scripts/units/carrier.js
+++ scripts/units/carrier.js
@@ -3,8 +3,8 @@ midway.constructor = () => extend(UnitWaterMove, {});
 //2 ain't enough. time to m u l t i p l y
 for(var i = 0; i < 5; i++){
   //Unit Spawn Ability(unit, spawn time, x, y)
-  midway.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 60 * 30, 0, -7.5));
-  midway.abilities.add(new UnitSpawnAbility(UnitTypes.flare, -60 * -30, 0, 7.5));
+  midway.abilities.add(new UnitSpawnAbility(UnitTypes.horizon, 60 * 30, 0, 15 - i * 7.5));
+  midway.abilities.add(new UnitSpawnAbility(UnitTypes.flare, 60 * 30, -7.50, 15 - i * 7.5));
 }
 //StatsFieldAbility(effect, duration, reload, radius)
 midway.abilties.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
