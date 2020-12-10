a8f149b545d10398cf6fba8aa08e5f56b0006005
MEEP of Faith <meepmatthew@gmail.com>
2020-12-10 10:28:20 -0800

    comments

--- scripts/units/akashi.js
+++ scripts/units/akashi.js
@@ -1,4 +1,6 @@
 const akashi = extendContent (UnitType, "akashi", {});
 akashi.constructor = () => extend(UnitWaterMove, {});
-akashi.abilities.add(new ForceFieldAbility(48, 1, 3000, 60 * 10));
-akashi.abilities.add(new RepairFieldAbility(60 * 9, 60 * 9, 46));
+//ForceFieldAbility(radius, regen (hp/tick), maxHp, cooldown (ticks to come back after being dstroyed))
+akashi.abilities.add(new ForceFieldAbility(48, 1, 3000, 60 * 15));
+//RepairFieldAbility(Amount, Reload, Radius)
+akashi.abilities.add(new RepairFieldAbility(50, 15, 46));
