7809b6b7b9be5348e4873d0d8af464ace761c585
Dragconic <60554123+K103908@users.noreply.github.com>
2020-12-10 13:09:05 -0500

    Update akashi.js

--- scripts/units/akashi.js
+++ scripts/units/akashi.js
@@ -1,4 +1,4 @@
-const midway = extendContent (UnitType, "akashi", {});
+const akashi = extendContent (UnitType, "akashi", {});
 akashi.constructor = () => extend(UnitWaterMove, {});
 akashi.abilities.add(new ForceFieldAbility(60 * 9, 60 * 9, 46));
 akashi.abilities.add(new RepairFieldAbility(60 * 9, 60 * 9, 46));
