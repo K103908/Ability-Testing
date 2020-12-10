b762b1d4507a67915dab8d6bd757ac4a9b711551
MEEP of Faith <meepmatthew@gmail.com>
2020-12-10 12:09:28 -0800

    gotcha

--- scripts/units/carrier.js
+++ scripts/units/carrier.js
@@ -9,7 +9,7 @@ for(var i = 0; i < 5; i++){
   Carrier.abilities.add(new UnitSpawnAbility(UnitTypes.flare, 60 * 30 + 10 * i + delay[(i + 1) % 2], sides[(i + 1) % 2], 32 - i * 16));
 }
 //StatsFieldAbility(effect, duration, reload, radius)
-Carrier.abilties.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
+Carrier.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
 
 //UnitFactory.UnitPlan(unit, time, cost)
 Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(Carrier, 60 * 25, ItemStack.with(Items.silicon, 69, Items.metaglass, 69, Items.titanium, 69)));
